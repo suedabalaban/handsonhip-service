2f2c6d375c4d64b05be74b6008f4261dda78715e
[suedabalaban] <suedabalaban@gmail.com>
2024-07-24 15:24:52 +0300

    Add h2database dependency

---
+++ app/src/main/java/com/handsonhip/HandsonhipApplication.java
@@ -0,0 +1,12 @@
+package com.handsonhip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsonhipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandsonhipApplication.class, args);
+    }
+}
