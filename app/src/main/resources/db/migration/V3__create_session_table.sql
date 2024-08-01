36d8427cd1e49fc9c96c10b9d7dcd875a23fb9fd
goktu-o <92593521+goktu-o@users.noreply.github.com>
2024-08-01 14:57:02 +0300

    Update V3__create_session_table.sql

--- app/src/main/resources/db/migration/V3__create_session_table.sql
+++ app/src/main/resources/db/migration/V3__create_session_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS session (
     id BIGSERIAL PRIMARY KEY, -- Changed to BIGSERIAL for potential larger ID values
     memberID BIGINT NOT NULL,
-    login_time TIMESTAMP NOT NULL,
+    logintime TIMESTAMP NOT NULL,
     CONSTRAINT fk_member FOREIGN KEY(memberID) REFERENCES member(memberID)
 );
